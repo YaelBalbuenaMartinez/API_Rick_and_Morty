8d3a03b6803ec8d95d24e14d897c776cb6ead10c
YaelBalbuenaMartinez <yael_1321124280@uptecamac.edu.mx>
2023-12-21 10:19:50 -0600

    Inicializar proyecto

---
+++ APIR-M/src/test/java/com/RM/APIR/M/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RM.APIR.M;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
